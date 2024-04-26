5a885a0c64b9cbcce3a2808ed1fafde572e93966
wrlu <wrlu.cn@outlook.com>
2024-04-26 10:25:57 +0800

    Check watched uid in front end side.

--- art/runtime/native/DexFileData.h
+++ art/runtime/native/DexFileData.h
@@ -15,8 +15,8 @@
 #include <sys/un.h>
 #include <utils/Log.h>
 
-
 #define IN_MEMORY_DEX_FILENAME "[anonymous]"
+
 #define FS_DATA_PATH "/data"
 #define FS_STORAGE_PATH "/storage"
 #define FS_SDCARD_PATH "/sdcard"
@@ -46,10 +46,11 @@ public:
         snprintf(headerStr, MAX_DEX_FILE_HEADER_LEN, "%d\n%d\n%s\n%ld\n%s\n", uid, pid, packageName.c_str(), timestamp, sourceName);
 
         int bytes_sent = send(fd, headerStr, strlen(headerStr), 0);
+        int err = 0;
         if (bytes_sent < 0) {
-            return -1;
+            err = -1;
         }        
-        return bytes_sent;
+        return err;
     }
 
     int writeDexFile(int fd) {
@@ -65,16 +66,18 @@ public:
         }
         uint8_t *buffer = new uint8_t[10240];
 
+        int err = 0;
         while (feof(sourceFd) != 1) {
             int c = fread(buffer, 1, 1024, sourceFd);
             int bytes_sent = send(fd, buffer, c, 0);
             if (bytes_sent != c) {
+                err = -1;
                 break;
             }
         }
         delete[] buffer;
         fclose(sourceFd);
-        return 0;
+        return err;
     }
 };
 
@@ -97,7 +100,7 @@ bool isHotfixDexPath(const char *sourceName) {
     if (strstart(sourceName, FS_DATA_APP_PATH)) {
         return false;
     }
-    // Otherwise should be a hot patch (like /data/user/0, /data/data, /sdcard or /storage/emulated/0).
+    // Otherwise should be a hot patch (like /data/user/0, /data/user_de/0 or /storage/emulated/0).
     return true;
 }
 
@@ -119,37 +122,60 @@ int connectAppSandboxDexUDS() {
     return -1;
 }
 
-int writeDex(const char *sourceName){
-    if (sourceName == nullptr || getuid() < 10000) {
+int checkWatchedUid(int uid) {
+    if (uid < 10000) {
         return -1;
     }
-    if (isHotfixDexPath(sourceName)) {
-        DexFileData dexFileData(getuid(), getpid(), time(nullptr), sourceName);
-        int udsFd = connectAppSandboxDexUDS();
-        dexFileData.writeBasicInfo(udsFd);
-        dexFileData.writeDexFile(udsFd);
-        close(udsFd);
-    }
     return 0;
-    
+}
+
+int writeDex(const char *sourceName){
+    if (sourceName == nullptr) {
+        return -1;
+    }
+    int selfUid = getuid();
+
+    if (checkWatchedUid(selfUid) != 0) {
+        return -1;
+    }
+
+    if (isHotfixDexPath(sourceName)) {
+        DexFileData dexFileData(selfUid, getpid(), time(nullptr), sourceName);
+        int udsFd = connectAppSandboxDexUDS();
+        int err = dexFileData.writeBasicInfo(udsFd);
+        if (err != 0) {
+            close(udsFd);
+            return err;
+        }
+        err = dexFileData.writeDexFile(udsFd);
+        close(udsFd);
+        return err;
+    }
+    return -1;
 }
 
 int writeInMemoryDex(void *buffer, size_t length) {
-    if (buffer == nullptr || length == 0 || getuid() < 10000) {
+    if (buffer == nullptr || length <= 0) {
         return -1;
     }
-    DexFileData dexFileData(getuid(), getpid(), time(nullptr), IN_MEMORY_DEX_FILENAME);
+    int selfUid = getuid();
+    if (checkWatchedUid(selfUid) != 0) {
+        return -1;
+    }
+    DexFileData dexFileData(selfUid, getpid(), time(nullptr), IN_MEMORY_DEX_FILENAME);
     int udsFd = connectAppSandboxDexUDS();
-    dexFileData.writeBasicInfo(udsFd);
+    int err = dexFileData.writeBasicInfo(udsFd);
+    if (err != 0) {
+        close(udsFd);
+        return err;
+    }
     // Send memory dex buffer directly.
     size_t bytes_sent = send(udsFd, buffer, length, 0);
     if (bytes_sent != length) {
-        close(udsFd);
-        return -1;
+        err = -1;
     }
-
     close(udsFd);
-    return 0;
+    return err;
 }
 
 }
